312c18da9165ac8dc399e08346b6d6e93c178f56
bobbybob2131 <66387977+bobbybob2131@users.noreply.github.com>
2022-03-29 18:54:35 +0100

    Delete DelayedDestroy.lua

--- DelayedDestroy.lua
+++
@@ -1,8 +0,0 @@
--- Destroy an Instance after `lifetime` seconds
-local function delayedDestroy(object: Instance, lifetime: number?)
-	task.delay(lifetime or 10, function()
-		if object then -- Might already be destroyed
-			object:Destroy()
-		end
-	end)
-end
