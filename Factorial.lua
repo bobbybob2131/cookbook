84d650ab8cfbc00de2397e6cb1a1dc1fef01e080
bobbybob2131 <66387977+bobbybob2131@users.noreply.github.com>
2022-03-29 18:51:33 +0100

    Delete Factorial.lua

--- Factorial.lua
+++
@@ -1,9 +0,0 @@
--- Get the factorial of a number (!x)
-local function factorial(number: number): number
-	local total: number = number
-	while not number == 0 do
-		number -= 1
-		total *= number
-	end
-	return total
-end
