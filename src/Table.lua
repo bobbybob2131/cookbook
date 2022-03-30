9cc6fc1c022156662a3d89cbbc010293de4270d0
bobbybob2131 <66387977+bobbybob2131@users.noreply.github.com>
2022-03-30 16:48:24 +0100

    Fix, add normalise tuple
    Fixed pretty concat func

--- src/Table.lua
+++ src/Table.lua
@@ -1,12 +1,17 @@
 local function prettyConcatenate(array: {[number]: any}): string
-	if #list == 1 then
-		return tostring(list[1])
-	elseif #list == 2 then
-		return list[1] .. " and " .. list[2]
+	if #array == 1 then
+		return tostring(array[1])
+	elseif #array == 2 then
+		return array[1] .. " and " .. array[2]
 	else
-		local lastItem = table.remove(list)
-		local text = table.concat(list, ", ") .. ", and " .. lastItem
-		table.insert(list, lastItem)
+		local lastItem: any = table.remove(array)
+		local text: string = table.concat(array, ", ") .. ", and " .. lastItem
+		table.insert(array, lastItem)
 		return text
 	end
+end
+
+-- Make sure a tuple (...) is a table
+local function normaliseTuple(...: any)
+	return if type(...) == "table" then ... else {...}
 end
