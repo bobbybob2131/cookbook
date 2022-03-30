94e6e6a57ef4d920919dff40270709da1a4ac97f
bobbybob2131 <66387977+bobbybob2131@users.noreply.github.com>
2022-03-30 17:52:38 +0100

    Rewrite
    Modularise, add docs

--- src/Instance.lua
+++ src/Instance.lua
@@ -1,8 +1,21 @@
+--!strict
+-- Author(s): bobbybob2131 
+-- Last edited: 30 March 2022
+-- Description: A function for Instances.
+
+--[[
+function instanceModule.delayedDestroy(object: Instance, lifetime: number?) Destroy an Instance after `lifetime` seconds
+]]
+
+local instanceModule = {}
+
 -- Destroy an Instance after `lifetime` seconds
-local function delayedDestroy(object: Instance, lifetime: number?)
+function instanceModule.delayedDestroy(object: Instance, lifetime: number?)
 	task.delay(lifetime or 10, function()
 		if object then -- Might already be destroyed
 			object:Destroy()
 		end
 	end)
 end
+
+return instanceModule
